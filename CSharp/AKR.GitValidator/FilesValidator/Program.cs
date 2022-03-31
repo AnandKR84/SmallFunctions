01d0311de40508c8817fcef7de1f501af0096059
Anand K R <anand.kr84@gmail.com>
2022-03-31 15:41:17 +0530

    Committing pending work

---
+++ CSharp/AKR.GitValidator/FilesValidator/Program.cs
@@ -0,0 +1,27 @@
+﻿using CommandLine;
+using System;
+
+namespace FilesValidator
+{
+    public class Program
+    {
+        public class Options
+        {
+            [Option('p', "path", Required = true, HelpText = "Path under which the json files need to search")]
+            public string GitPath
+            {
+                get; set;
+            }
+
+            [Option('r', "recurse", Required = false, HelpText = "Recurse the sub-folders and find the files. Default = true")]
+            public bool Recurse { get; set; } = true;
+        }
+
+        private static void Main(string[] args)
+        {
+            Parser.Default.ParseArguments<Options>(args).WithParsed<Options>{
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
